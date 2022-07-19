4e5bb5347018efc41dfc9b793200d1fe1106c82c
Jafet <febmerlib@gmail.com>
2022-07-19 00:13:18 +0000

    fix getCookies

--- api.tsx
+++ api.tsx
@@ -5,43 +5,44 @@ import { getCookie } from './utils/Cookies';
 
 
 type Fetching = {
-    graphql : CallableFunction
-    youtube : CallableFunction
-    restCountries : CallableFunction
+    graphql: CallableFunction
+    youtube: CallableFunction
+    restCountries: CallableFunction
     socketIO: CallableFunction
 }
 
 
 
-const instance : AxiosInstance = axios.create({baseURL: process.env.NEXT_PUBLIC_BASE_URL})
+const instance: AxiosInstance = axios.create({ baseURL: process.env.NEXT_PUBLIC_BASE_URL })
 
 
-export const api : Fetching = {
-    graphql : async (data: object, token: string) : Promise<AxiosResponse> => {
-        let tokenFinal : string | null = token ||  Cookies.get("idToken") || ""
-            return await instance.post("/graphql", data, {
-                headers: {
-                    Authorization : `Bearer ${tokenFinal}`
-                }
-            })
-    },
-    youtube : async (data: any) : Promise<AxiosResponse> => {
-        return await axios.get("https://www.googleapis.com/youtube/v3/search", {
-            params: {
-                key : process.env.NEXT_PUBLIC_API_KEY_CONSOLE_GOOGLE,
-                part : "id,snippet",
-                order : "date",
-                channelId : "UCuQNm4bt_zQc5miwSm4WYXA"
+export const api: Fetching = {
+    graphql: async (data: object, token: string): Promise<AxiosResponse> => {
+        //let tokenFinal : string | null = token ||  Cookies.get("idToken") || ""
+        const tokenFinal = Cookies.get("idToken")
+        return await instance.post("/graphql", data, {
+            headers: {
+                Authorization: `Bearer ${tokenFinal}`
             }
         })
     },
-    restCountries: async () : Promise<AxiosResponse> => {
+    youtube: async (data: any): Promise<AxiosResponse> => {
+        return await axios.get("https://www.googleapis.com/youtube/v3/search", {
+            params: {
+                key: process.env.NEXT_PUBLIC_API_KEY_CONSOLE_GOOGLE,
+                part: "id,snippet",
+                order: "date",
+                channelId: "UCuQNm4bt_zQc5miwSm4WYXA"
+            }
+        })
+    },
+    restCountries: async (): Promise<AxiosResponse> => {
         return await axios.get('https://restcountries.com/v3.1/all')
     },
 
-    socketIO : ({token} : {token: string}) => {
+    socketIO: ({ token }: { token: string }) => {
         const socket = io(`https://api.bodasdehoy.com`, {
-            auth:{
+            auth: {
                 token: `Bearer ${token}`
             }
         })

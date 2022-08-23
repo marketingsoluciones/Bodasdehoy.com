06daaeafdd7034679d01cfd13ab3ea49d1a16621
Jafet <febmerlib@gmail.com>
2022-08-23 15:14:30 +0000

    BODAPP-88 add useOutsideSetShow

--- components/Chat/ChatComponent.tsx
+++ components/Chat/ChatComponent.tsx
@@ -27,6 +27,22 @@ import { getRelativeTime } from "../../utils/FormatTime";
 import { createURL } from "../../utils/UrlImage";
 import { SetStateAction } from "react";
 
+const useOutsideAlerter = (ref: any, setShow: any) => {
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setShow(false)
+      //alert('¡Hiciste clic fuera de mí!');
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+};
+
 const initialState = { state: false, data: null };
 
 const SearchChat: FC<any> = ({ onChange }) => {
@@ -53,12 +69,15 @@ const SearchChat: FC<any> = ({ onChange }) => {
 
 const ChatComponent = () => {
   const { user } = AuthContextProvider();
-  
+
   const { conversation, setConversation, show, setShow } = ChatContextProvider();
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef, setShow);
   return (
     user && (
       <>
-        <div
+        <div ref={wrapperRef}
           className={`sm:w-96 sm:h-96 w-full h-full chat bottom-0 sm:block bg-white shadow-lg fixed sm:right-5 z-40 sm:sm:rounded-t-xl ${show ? "translate-y-0" : "sm:translate-y-[90%] translate-y-[100%]"} transition duration-500`}
         >
           <div className="w-full h-[87%] md:h-full relative sm:rounded-t-xl">
@@ -72,9 +91,8 @@ const ChatComponent = () => {
               </div>
 
               <ArrowIcon
-                className={`w-4 h-4 text-white transform ${
-                  show ? "rotate-90" : "-rotate-90"
-                }`}
+                className={`w-4 h-4 text-white transform ${show ? "rotate-90" : "-rotate-90"
+                  }`}
               />
             </div>
 
@@ -88,15 +106,15 @@ const ChatComponent = () => {
             )}
           </div>
         </div>
-       <style jsx>
-         {`
+        <style jsx>
+          {`
           @media (max-width: 639px) {
             .chat {
               top: 0
             }
           }
          `}
-       </style>
+        </style>
       </>
     )
   );
@@ -145,22 +163,22 @@ const ListChats: FC<propsListChats> = ({ show, setShow }) => {
           className="flex flex-col overflow-auto h-[19rem] no-scrollbar"
         >
           {chats?.results?.length > 0 ? chats?.results?.map((item: Chat, idx: number) => (
-              <ConversationItem
-                key={idx}
-                {...item}
-                onClick={() =>
-                  setConversation({
-                    state: true,
-                    data: item,
-                  })
-                }
-              />
-            ))
-           : (
-            <div className="text-primary h-full w-full flex items-center justify-center top-0 left-0 bg-white">
-              <EmptyComponent text={"No hay chats"} />
-            </div>
-          )}
+            <ConversationItem
+              key={idx}
+              {...item}
+              onClick={() =>
+                setConversation({
+                  state: true,
+                  data: item,
+                })
+              }
+            />
+          ))
+            : (
+              <div className="text-primary h-full w-full flex items-center justify-center top-0 left-0 bg-white">
+                <EmptyComponent text={"No hay chats"} />
+              </div>
+            )}
         </div>
       ) : (
         <div className="text-primary h-full w-full flex items-center justify-center top-0 left-0 bg-white">
@@ -224,9 +242,8 @@ const ConversationItem: FC<propsConversationItem> = ({ onClick, ...props }) => {
           }
         />
         <span
-          className={`absolute w-3 h-3 ${
-            isOnline ? "bg-green-600" : "bg-gray-400"
-          }  rounded-full -right-0 border border-white top-0`}
+          className={`absolute w-3 h-3 ${isOnline ? "bg-green-600" : "bg-gray-400"
+            }  rounded-full -right-0 border border-white top-0`}
         />
       </span>
       <div className="flex flex-col">
@@ -312,55 +329,55 @@ const ModuleChat: FC<propsModuleChat> = ({ setConversation, data }) => {
 
   return (
     <>
-    <div className=" flex-col p-3 h-full absolute top-0 left-0 w-full z-10 bg-white sm:rounded-t-xl ">
-      <HeaderChat data={data} setConversation={setConversation} />
-      {/* BODY */}
-      <div
-        ref={refBoxMsg}
-        className="moduleChat w-full flex flex-col gap-5 overflow-auto px-5 pb-6"
-      >
-        {!loading ? (
-          // @ts-ignore
-          messages?.messages?.map(({ emitUserUid, ...message }, idx: number) => (
+      <div className=" flex-col p-3 h-full absolute top-10 left-0 w-full z-10 bg-white sm:rounded-t-xl ">
+        <HeaderChat data={data} setConversation={setConversation} />
+        {/* BODY */}
+        <div
+          ref={refBoxMsg}
+          className="moduleChat w-full flex flex-col gap-5 overflow-auto px-5"
+        >
+          {!loading ? (
+            // @ts-ignore
+            messages?.messages?.map(({ emitUserUid, ...message }, idx: number) => (
               <MessageItem
                 key={idx}
                 isSender={user?.uid === emitUserUid}
                 {...message}
               />
             )
-          )
-        ) : (
-          <span className="text-gray-400 w-full h-full flex items-center justify-center">
-            <LoadingItem size="small" text="" />
-          </span>
-        )}
+            )
+          ) : (
+            <span className="text-gray-400 w-full h-full flex items-center justify-center">
+              <LoadingItem size="small" text="" />
+            </span>
+          )}
+        </div>
+        {/* INPUT */}
+        <form className="relative w-full mt-2" onSubmit={handleClick}>
+          <input
+            onChange={handleChangeInput}
+            value={value}
+            className="focus:outline-none bg-white h-10 border rounded-lg w-full pr-10 pl-3 text-sm"
+          />
+          <button onClick={handleClick}>
+            <svg
+              className="cursor-pointer w-6 h-6 rotate-90 hover:bg-color-base transition p-0.5 absolute inset-y-0 my-auto right-2 text-primary rounded-full "
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+            </svg>
+          </button>
+        </form>
       </div>
-      {/* INPUT */}
-      <form className="relative w-full mt-2" onSubmit={handleClick}>
-        <input
-          onChange={handleChangeInput}
-          value={value}
-          className="focus:outline-none bg-white h-10 border rounded-lg w-full pr-10 pl-3 text-sm"
-        />
-        <button onClick={handleClick}>
-          <svg
-            className="cursor-pointer w-6 h-6 rotate-90 hover:bg-color-base transition p-0.5 absolute inset-y-0 my-auto right-2 text-primary rounded-full "
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-          </svg>
-        </button>
-      </form>
-    </div>
-    <style jsx>
-      {`
+      <style jsx>
+        {`
         .moduleChat{
-          height: calc(100% - 7rem)
+          height: calc(100% - 7rem - 40px)
         }
       `}
-    </style>
+      </style>
     </>
   );
 };
@@ -386,7 +403,7 @@ const HeaderChat: FC<propsModuleChat> = ({ data, setConversation }) => {
     };
   }, [socket, data?.addedes]);
   return (
-    <div className="p-2 border-b border-color-base mb-3 w-full flex items-center justify-between">
+    <div className="p-2 border-b border-color-base w-full flex items-center justify-between">
       <div className="flex items-center gap-2">
         <ImageProfile isActived={isOnline} image={data?.photoURL} />
         <div className="flex items-start flex-col justify-end">
@@ -434,23 +451,19 @@ const MessageItem: FC<Partial<propsMessageItem>> = ({
   };
   return (
     <div
-      className={`flex flex-col gap-[0.2rem] relative ${
-        isSender ? "items-end" : "items-start"
-      }`}
+      className={`flex flex-col gap-[0.2rem] relative ${isSender ? "items-end" : "items-start"
+        }`}
     >
       <p
-        className={`p-2 ${
-          isSender ? "bg-primary" : "bg-gray-400"
-        } rounded-md text-xs w-max text-white cursor-pointer`}
+        className={`p-2 ${isSender ? "bg-primary" : "bg-gray-400"
+          } rounded-md text-xs w-max text-white cursor-pointer`}
         onClick={handleClick}
       >
         {message}
       </p>
 
       <small
-        className={`${
-          dateView ? "opacity-100" : "opacity-0"
-        } text-[0.65rem] text-gray-600  absolute -bottom-0.5 transform translate-y-full transition-all`}
+        className={` text-[0.65rem] text-gray-600  absolute -bottom-0.5 transform translate-y-full transition-all`}
       >
         {/* {createdAt && typeof createdAt === "string" ? getRelativeTime(Date(createdAt)) : getRelativeTime(createdAt)} */}
         {createdAt &&

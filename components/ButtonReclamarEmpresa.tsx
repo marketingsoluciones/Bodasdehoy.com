08577866f5b443297ff38b6e4c55733da1b432e0
eduardo andres <adch.edch@gmail.com>
2022-09-19 14:58:58 -0500

    implementacion de formulario para reclamar empresas v2

--- components/ButtonReclamarEmpresa.tsx
+++ components/ButtonReclamarEmpresa.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import { CrossIcon, UserIcon } from './Icons'
 import Router from 'next/router'
+import ModalReclarEmpresa from './ReclamarEmpresa/ModalReclamarEmpresa'
+import { useState } from 'react'
 
 const ButtonReclamarEmpresa = () => {
+    const [showForm, setShowForm] = useState(true)
     return (
         <>
-        
+            {showForm ? (
+                <ModalReclarEmpresa set={setShowForm} state={showForm} />
+            ) : null}
+
             <button
                 className={`hidden sm:flex cursor-pointer bg-color-base font-semibold rounded-full w-max px-4 text-xs py-2 text-primary fixed bottom-16 right-6 z-30 items-center gap-2  transition-all duration-500 shadow-lg`}
-                onClick={()=> Router.push("/info-empresa")}
+                /* onClick={() => Router.push("/reclama-tu-empresa")} */
+                onClick={()=>setShowForm(!showForm)}
             >
                 <CrossIcon />
                 Reclama tu empresa aquí
--- components/ReclamarEmpresa/ModalReclamarEmpresa.jsx
+++ components/ReclamarEmpresa/ModalReclamarEmpresa.jsx
@@ -2,16 +2,16 @@ import ClickAwayListener from "react-click-away-listener"
 import Script from "next/script"
 import Router from 'next/router'
 
-const ModalReclarEmpresa = ({set, state, children}) => {
+const ModalReclarEmpresa = ({set, state,}) => {
     return (
         <>
 
 
-            <div className=" backdrop-filter backdrop-blur z-30 fixed top-0 left-0 w-screen h-screen overflow-hidden " />
-            <div className=" bg-black opacity-60 z-30 fixed top-0 left-0 w-screen h-screen overflow-hidden " />
+            <div className=" backdrop-filter backdrop-blur z-50 fixed top-0 left-0 w-screen h-screen overflow-hidden " />
+            <div className=" bg-black opacity-60 z-50 fixed top-0 left-0 w-screen h-screen overflow-hidden " />
 
             <ClickAwayListener onClickAway={() => state && set(!state) }>
-                <div className="bg-white w-max h-max shadow-lg fixed m-auto inset-0 z-30 rounded-xl px-10  py-5">
+                <div className="bg-white w-max h-4/5 shadow-lg fixed m-auto inset-0 z-50 rounded-xl px-10  py-5">
                     <div className="pipedriveWebForms " data-pd-webforms="https://webforms.pipedrive.com/f/clNHJGQZCUUdzuoyeZgofOkJQMFhqWEqwEo77zrn4uamAuTsh0cDvHwXG9G7Yk287N">
                         <Script
                             src="https://webforms.pipedrive.com/f/loader"

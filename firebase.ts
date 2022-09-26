37b6760aabb100c2ac0925267c026650f49eed1b
eduardo andres <adch.edch@gmail.com>
2022-09-26 15:03:48 -0500

    prueba para verificacion de firebaseconfig

--- firebase.ts
+++ firebase.ts
@@ -1,27 +1,40 @@
-import { initializeApp, getApp } from "firebase/app";
-import { GoogleAuthProvider, FacebookAuthProvider, getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { GoogleAuthProvider, FacebookAuthProvider, getAuth } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { getStorage, ref, uploadBytes } from "firebase/storage"
 import { string } from "yup";
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDVMoVLWWvolofYOcTYA0JZ0QHyng72LAM",
+const otherAppConfig = {
+  /* apiKey: "AIzaSyDVMoVLWWvolofYOcTYA0JZ0QHyng72LAM",
   authDomain: "bodasdehoy-1063.firebaseapp.com",
   databaseURL: "https://bodasdehoy-1063-default-rtdb.firebaseio.com",
   projectId: "bodasdehoy-1063",
   storageBucket: "bodasdehoy-1063.appspot.com",
   messagingSenderId: "593952495916",
   appId: "1:593952495916:web:c63cf15fd16a6796f6f489",
-  measurementId: "G-GWQ17NF2YR",
+  measurementId: "G-GWQ17NF2YR", */
+
+  apiKey: "AIzaSyCRxkvW5Vgsa5obq7b4Peslp_RN0PUjmys",
+  authDomain: "auth-bodas.firebaseapp.com",
+  projectId: "auth-bodas",
+  storageBucket: "auth-bodas.appspot.com",
+  messagingSenderId: "856588028724",
+  appId: "1:856588028724:web:cae47a47225f86f6037361"
+
 };
 
-const firebaseClient = initializeApp(firebaseConfig);
+if (initializeApp.length === 0) {
+  initializeApp({})
+}
+
+const firebaseClient = initializeApp(otherAppConfig);
 console.log(getApp().name);
 const auth = getAuth()
 //const storage = getStorage();
 
 //Providers
+
 const GoogleProvider = () => {
   const provider = new GoogleAuthProvider();
   provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
@@ -30,7 +43,7 @@ const GoogleProvider = () => {
 
 const FacebookProvider = new FacebookAuthProvider();
 
-export { firebaseClient, GoogleProvider, FacebookProvider, auth };
+export { otherAppConfig, firebaseClient, GoogleProvider, FacebookProvider, auth };
 
 
 

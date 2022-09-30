738f6c01d17a4e0614a2c4b9629febb23f5e4c52
Jafet <febmerlib@gmail.com>
2022-09-30 04:25:39 +0000

    reverse

--- firebase.ts
+++ firebase.ts
@@ -6,21 +6,21 @@ import { string } from "yup";
 
 
 const otherAppConfig = {
-  // apiKey: "AIzaSyDVMoVLWWvolofYOcTYA0JZ0QHyng72LAM",
-  // authDomain: "bodasdehoy-1063.firebaseapp.com",
-  // databaseURL: "https://bodasdehoy-1063-default-rtdb.firebaseio.com",
-  // projectId: "bodasdehoy-1063",
-  // storageBucket: "bodasdehoy-1063.appspot.com",
-  // messagingSenderId: "593952495916",
-  // appId: "1:593952495916:web:c63cf15fd16a6796f6f489",
-  // measurementId: "G-GWQ17NF2YR",
+  apiKey: "AIzaSyDVMoVLWWvolofYOcTYA0JZ0QHyng72LAM",
+  authDomain: "bodasdehoy-1063.firebaseapp.com",
+  databaseURL: "https://bodasdehoy-1063-default-rtdb.firebaseio.com",
+  projectId: "bodasdehoy-1063",
+  storageBucket: "bodasdehoy-1063.appspot.com",
+  messagingSenderId: "593952495916",
+  appId: "1:593952495916:web:c63cf15fd16a6796f6f489",
+  measurementId: "G-GWQ17NF2YR",
 
-  apiKey: "AIzaSyCRxkvW5Vgsa5obq7b4Peslp_RN0PUjmys",
-  authDomain: "auth-bodas.firebaseapp.com",
-  projectId: "auth-bodas",
-  storageBucket: "auth-bodas.appspot.com",
-  messagingSenderId: "856588028724",
-  appId: "1:856588028724:web:cae47a47225f86f6037361"
+  // apiKey: "AIzaSyCRxkvW5Vgsa5obq7b4Peslp_RN0PUjmys",
+  // authDomain: "auth-bodas.firebaseapp.com",
+  // projectId: "auth-bodas",
+  // storageBucket: "auth-bodas.appspot.com",
+  // messagingSenderId: "856588028724",
+  // appId: "1:856588028724:web:cae47a47225f86f6037361"
 
 };
 

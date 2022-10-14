95bfe233d0d796c4e1414fac0448ce5934251ad2
eduardo andres <adch.edch@gmail.com>
2022-10-14 11:56:43 -0500

    implementar cambios en el login por verificacion de google bodapp-178

--- components/Login/Components.tsx
+++ components/Login/Components.tsx
@@ -61,11 +61,11 @@ export const Providers: FC<any> = ({ setStage }) => {
     } catch (error) {
       setLoading(false);
       toast("error", JSON.stringify(error));
-      console.log("este es un error en el onClick de los listProviders",error);
+      console.log("este es un error en el onClick de los listProviders", error);
     }
   };
 
-  const ListProviders = [
+  /* const ListProviders = [
     {
       icon: <FacebookIcon className="w-5 h-5" />,
       function: () => handleClick(FacebookProvider),
@@ -78,17 +78,63 @@ export const Providers: FC<any> = ({ setStage }) => {
       icon: <AppleIcon className="w-5 h-5" />,
       function: () => alert("Aun por configurar"),
     },
-  ];
+  ]; */
 
   return (
-    <div className={`text-center flex flex-col gap-2 w-full items-center `}>
-      <h1 className="text-primary">Accede con</h1>
-      <div className="gap-4 flex items-center">
+    <>
+      <div className={`text-center flex flex-col gap-2 w-full items-center `}>
+        <h1 className="text-primary">Accede con</h1>
+        {/* <div className="gap-4 flex items-center">
         {ListProviders.map((item, idx) => (
           <Icon key={idx} icon={item.icon} onClick={item.function} />
         ))}
+      </div> */}
+        <div className="gap-4 flex items-center  ">
+          <span className="h-10 w-10 bgColorFc  flex items-center justify-center rounded  ">
+            <button onClick={() => handleClick(FacebookProvider)} className="facebookBtn" />
+          </span>
+
+          <span className=" h-10 w-10  flex items-center justify-center rounded bg-color-base">
+            <button onClick={() => handleClick(GoogleProvider())} className="googleBtn" />
+          </span>
+
+          <span className="h-10 w-10   flex items-center justify-center raunded  bg-color-base">
+            <button onClick={() => alert("AUN POR CONFIGURAR")} className="AppleBtn" />
+          </span>
+        </div>
       </div>
-    </div>
+      <style jsx>
+        {`
+          .googleBtn {
+            background-image: url("/google-logo-color.svg");
+            width: 17px;
+            height: 18px;
+            opacity: 1;
+            background-repeat: no-repeat;
+            }
+          .facebookBtn {
+            background-image: url("/facebook-logo-white.svg");
+            width: 17px;
+            height: 18px;
+            opacity: 1;
+            background-repeat: no-repeat;
+            psition: absolute;
+          }
+          .bgColorFc{
+            background-color:#1877F2
+            
+          }
+          .AppleBtn {
+            background-image: url("/apple-logo-black.svg");
+            width: 15px;
+            height: 19px;
+            opacity: 1;
+            background-repeat: no-repeat;
+            psition: absolute;
+          }
+        `}
+      </style>
+    </>
   );
 };
 

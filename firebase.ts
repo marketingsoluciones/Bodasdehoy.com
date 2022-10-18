c8f6086fecf0cf5cfcd3029027cc0c12f9084cd3
Jafet <febmerlib@gmail.com>
2022-10-18 01:52:57 +0000

    modify firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -54,7 +54,7 @@ const auth = getAuth()
 
 const GoogleProvider = () => {
   const provider = new GoogleAuthProvider();
-  provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+  //provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
   return provider;
 };
 

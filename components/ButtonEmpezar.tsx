eac5980d4cceac0de3df6150e0099cd502333472
eduardo andres <adch.edch@gmail.com>
2022-11-15 11:59:19 +0500

    fix error en boton empezar

--- components/ButtonEmpezar.tsx
+++ components/ButtonEmpezar.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { CrossIcon, UserIcon } from './Icons'
 import { useHover } from '../hooks/useHover';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const ButtonEmpezar = () => {
 
@@ -10,69 +11,67 @@ const ButtonEmpezar = () => {
 
     const router = useRouter()
 
-    const {user} = AuthContextProvider()
+    const { user } = AuthContextProvider()
 
     const handleClick = async () => {
-        
-       
-            router.push("/login")
-      
+
+
+        router.push("/login")
+
     }
 
-  return (
-    <>
-    {(()=>{
-        if (!user) {
-            return (
-                <div className="flex cursor-pointer bg-primary font-semibold rounded-full w-max px-4 text-xs py-2 text-color-base fixed md:bottom-20 bottom-44 right-1 md:right-6 z-30 items-center gap-2  transition-all duration-500 shadow-lg  ">
-                    
-                    
-                            {/* <button className=" fixed bg-primary text-white text-sm py-2  px-4 rounded-full w-full mt-10 "> */}
-                                Empezar Gratis
-                          {/*   </button> */}
-                        
-                    
-                </div>
-            )
-        } else {
-        
-        }
+    return (
+        <>
+            {(() => {
+                if (!user) {
+                    return (
+                        <div className="flex cursor-pointer bg-primary font-semibold rounded-full w-max px-4 text-xs py-2 text-color-base fixed md:bottom-20 bottom-44 right-1 md:right-6 z-30 items-center gap-2  transition-all duration-500 shadow-lg  ">
+                            <Link href={"/login"} passHref>
+                                <a>
+                                    Empezar Gratis
+                                </a>
+                            </Link>
+                        </div>
+                    )
+                } else {
 
-    })()}
+                }
 
-    {/* <div ref={hoverRef} onClick={handleClick} className={`hidden sm:flex cursor-pointer bg-color-base font-semibold rounded-full w-max px-4 text-xs py-2 text-primary fixed bottom-10 right-6 z-30 items-center gap-2  transition-all duration-500 shadow-lg`}>
+            })()}
+
+            {/* <div ref={hoverRef} onClick={handleClick} className={`hidden sm:flex cursor-pointer bg-color-base font-semibold rounded-full w-max px-4 text-xs py-2 text-primary fixed bottom-10 right-6 z-30 items-center gap-2  transition-all duration-500 shadow-lg`}>
         <CrossIcon/>
         Publica tu empresa aquí
     </div> */}
-    </>
-  )
-
-
-
-
-   /*  const user = AuthContextProvider()
-    return (
-        <>        
-            {
-                (() => {
-                    if (!user) {
-                        return (
-                            <div className="flex fixed -bottom-10 right-6 w-full z-30  ">
-                                <Link href={"/login"} passHref>
-                                
-                                        <button className=" fixed bg-primary text-white text-sm py-2  px-4 rounded-full w-full mt-10 ">
-                                            Empezar
-                                        </button>
-                                    
-                                </Link>
-                            </div>
-                        )
-                    } else {
-                    
-                    }
-                })()}
         </>
-    ) */
+    )
+
+
+
+
+    /*  const user = AuthContextProvider()
+     return (
+         <>        
+             {
+                 (() => {
+                     if (!user) {
+                         return (
+                             <div className="flex fixed -bottom-10 right-6 w-full z-30  ">
+                                 <Link href={"/login"} passHref>
+                                 
+                                         <button className=" fixed bg-primary text-white text-sm py-2  px-4 rounded-full w-full mt-10 ">
+                                             Empezar
+                                         </button>
+                                     
+                                 </Link>
+                             </div>
+                         )
+                     } else {
+                     
+                     }
+                 })()}
+         </>
+     ) */
 }
 
 export default ButtonEmpezar 

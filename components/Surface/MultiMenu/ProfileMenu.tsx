9ac476621636a0bde11eb0374cbd1889201a9618
eduardo andres <adch.edch@gmail.com>
2023-09-19 10:22:39 -0400

    BODAPP-697 quitando mis post del menu

--- components/Surface/MultiMenu/ProfileMenu.tsx
+++ components/Surface/MultiMenu/ProfileMenu.tsx
@@ -56,12 +56,12 @@ export const ProfileMenu: FC<any> = ({ isHovered, setHovered, modal, setModal })
     //   icon: <WeddingPage />,
     //   rol: ["empresa"],
     // },
-    {
+    /* {
       title: "Mis post",
       onClick: async () => { await router.push(`${process.env.NEXT_PUBLIC_CMS}/posts`) },
       icon: <Posts />,
       rol: ["novio", "novia", "otro", "empresa"],
-    },
+    }, */
     {
       title: "Mis eventos",
       onClick: async () => { await router.push(`/welcome-app`) },

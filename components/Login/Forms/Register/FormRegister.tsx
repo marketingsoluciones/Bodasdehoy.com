20d26227b21b2bf1b602977938460834aad6015a
Jafet <febmerlib@gmail.com>
2023-11-04 02:08:17 +0100

    fixed

--- components/Login/Forms/Register/FormRegister.tsx
+++ components/Login/Forms/Register/FormRegister.tsx
@@ -167,18 +167,23 @@ const FormRegister: FC<propsFormRegister> = ({ whoYouAre, setStageRegister, stag
       }
 
       /////// REDIRECIONES ///////
-      if (redirect?.split("/")[3] == "info-empresa" && moreInfo.role.includes("empresa")) {
-        const path = window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") : process.env.NEXT_PUBLIC_EVENTSAPP
-        await router.push(path ?? "")
-        toast("success", `Inicio de sesión de empresa con exito `)
-      }
-      if (redirect?.split("/")[3] == "info-empresa" && !moreInfo.role.includes("empresa")) {
-        await router.push(redirect)
-        toast("warning", `Inicio sesión con una cuenta que no es de empresa`)
-      }
-      if (redirect?.split("/")[3] !== "info-empresa") {
-        await router.push(redirect ? redirect : "/")
+      if (router?.query?.end) {
+        await router.push(`${router?.query?.end}`)
         toast("success", `Inicio sesión con exito`)
+      } else {
+        if (router?.query?.d == "info-empresa" && moreInfo.role.includes("empresa")) {
+          const path = window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_CMS?.replace("//", "//test") : process.env.NEXT_PUBLIC_CMS
+          await router.push(path ?? "")
+          toast("success", `Cuenta de empresa creada con exito`)
+        }
+        if (router?.query?.d == "info-empresa" && !moreInfo.role.includes("empresa")) {
+          await router.push("/info-empresa")
+          toast("warning", `Inicio sesión con una cuenta que no es de empresa`)
+        }
+        if (router?.query?.d !== "info-empresa") {
+          await router.push(redirect ? redirect : "/")
+          toast("success", `Cuenta creada con exito`)
+        }
       }
       ///////////////////////////
 
--- utils/Authentication.tsx
+++ utils/Authentication.tsx
@@ -88,18 +88,18 @@ export const useAuthentication = () => {
               await router.push(`${router?.query?.end}`)
               toast("success", `Inicio sesión con exito`)
             } else {
-              // if (redirect?.split("/")[3] == "info-empresa" && moreInfo.role.includes("empresa")) {
-              //   const path = window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_EVENTSAPP?.replace("//", "//test") : process.env.NEXT_PUBLIC_EVENTSAPP
-              //   await router.push(path ?? "")
-              //   toast("success", `Inicio de sesión de empresa con exito1 `)
-              // }
-              // if (redirect?.split("/")[3] == "info-empresa" && !moreInfo.role.includes("empresa")) {
-              //   await router.push(redirect)
-              //   toast("warning", `Inicio sesión con una cuenta que no es de empresa1`)
-              // }
-              if (redirect?.split("/")[3] !== "info-empresa") {
+              if (router?.query?.d == "info-empresa" && moreInfo.role.includes("empresa")) {
+                const path = window.origin.includes("://test.") ? process.env.NEXT_PUBLIC_CMS?.replace("//", "//test") : process.env.NEXT_PUBLIC_CMS
+                await router.push(path ?? "")
+                toast("success", `Inicio de sesión de empresa con exito`)
+              }
+              if (router?.query?.d == "info-empresa" && !moreInfo.role.includes("empresa")) {
+                await router.push("/info-empresa")
+                toast("warning", `Inicio sesión con una cuenta que no es de empresa`)
+              }
+              if (router?.query?.d !== "info-empresa") {
                 await router.push(redirect ? redirect : "/")
-                toast("success", `Inicio sesión con exito1`)
+                toast("success", `Inicio sesión con exito`)
               }
             }
             ///////////////////////////

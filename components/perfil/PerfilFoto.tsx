78b80cfcd00119d004ab1a8491a0cf270f568ba0
eduardo andres <adch.edch@gmail.com>
2024-05-20 15:40:13 +0000

    working

--- components/perfil/PerfilFoto.tsx
+++ components/perfil/PerfilFoto.tsx
@@ -17,7 +17,7 @@ export const PerfilFoto = () => {
             <input type="file" id="photo" name="photo" className="hidden" />
             <h2 className="text-xl font-semibold w-full text-center text-tertiary">{user?.displayName}</h2>
             <div className="w-fit flex items-center gap-1 text-tertiary -mt-3">
-                <small className="text-gray-700">{user?.role && user.role.length > 0 && user?.role[0]}</small>
+                <small className="text-gray-700  ">{user?.role && user.role.length > 0 && user?.role[0]}</small>
                 |
                 <small>{user?.city}</small>
             </div>

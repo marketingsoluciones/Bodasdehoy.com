5c2b2d0c15bca968e066b44d10e2d0ed40f8589c
eduardo andres <adch.edch@gmail.com>
2024-08-19 14:42:53 -0400

    perfil options hidden

--- pages/configuracion.tsx
+++ pages/configuracion.tsx
@@ -87,17 +87,17 @@ const Configuration = () => {
         <AlertDesarrollo alertDev={modal} setAlertDev={setModal} />
       ) : null}
 
-      <section className="max-w-screen-lg mx-auto inset-x-0 grid grid-cols-1 md:grid-cols-4 md:pt-10 -mt-4 md:mt-0 md:gap-10">
+      <section className="max-w-screen-lg mx-auto inset-x-0 grid grid-cols-1 md:grid-cols-4* md:pt-10 -mt-4 md:mt-0 md:gap-10">
         <div className="flex flex-col items-center justify-start w-full text-sm gap-6">
           <PerfilFoto />
           <button onClick={handleSignOut} className="bg-red-500 px-3 py-1 rounded text-white text-sm sm:hidden top-2 left-2 flex items-center gap-2"><ExitIcon /> Cerrar sesión</button>
-          <PerfilOpciones
+          {/* <PerfilOpciones
             components={components}
             actived={isActive}
             onClick={handleClickOption}
             setAlertDev={setModal}
             alertDev={modal}
-          />
+          /> */}
         </div>
         <div className="col-span-3 p-5 md:p-0">{components[isActive].component}</div>
       </section>
